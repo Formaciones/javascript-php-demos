a09a3c86dd98f2a08b512519efb99c56082670e5
Borja Cabeza <borja.cabeza@programadores.net>
2026-05-14 20:27:06 +0200

    14-05 20.26

--- sentencias-de-control/ficha.php
+++ sentencias-de-control/ficha.php
@@ -1,10 +1,11 @@
 <?php 
     require "clientes.php";
 
-    $id = ; // Leido de parámetro id de la URL
-    $cliente = $clientes[$id];
+    $id = (int) ($_GET["id"] ?? "0"); // Leido de parámetro id de la URL
+    $cliente = $clientes[$id - 1];
 
-    $demo = "Demostración";
+    $provincias = array_unique(array_column($clientes, 'provincia'));
+    sort($provincias);
 ?>
 
 <!DOCTYPE html>
@@ -12,7 +13,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ficha Cliente | </title>
+    <title>Ficha Cliente | <?= htmlspecialchars($cliente["nombre"] ?? '') ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
 </head>
 <body>
@@ -20,7 +21,7 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <h1>Ficha de </h1>
+                <h1>Ficha de <?= htmlspecialchars($cliente["nombre"] ?? '') ?></h1>
                 <hr />
             </div>
         </div>
@@ -33,7 +34,7 @@
                             <label><b>Identificador</b></label>
                         </div>                        
                         <div class="col-9">
-                            <input type="text" nam="nombre" class="form-control" value="<?= htmlspecialchars($demo ?? '') ?>" placeholder="" />
+                            <input type="text" nam="id" class="form-control" value="<?= htmlspecialchars($cliente["id"] ?? '') ?>" placeholder="" />
                         </div>
                     </div>
                     <div class="row mb-3">
@@ -41,7 +42,7 @@
                             <label><b>Nombre y Apellidos</b></label>
                         </div>                        
                         <div class="col-9">
-                            <input type="text" class="form-control" value="" placeholder="" />
+                            <input type="text" name="nombre" class="form-control" value="<?= htmlspecialchars($cliente["nombre"] ?? '') ?>" placeholder="" />
                         </div>
                     </div>
                     <div class="row mb-3">
@@ -49,7 +50,7 @@
                             <label><b>Edad</b></label>
                         </div>                        
                         <div class="col-9">
-                            <input type="text" class="form-control" value="" placeholder="" />
+                            <input type="number" min="15" max="130" name="edad" class="form-control" value="<?= htmlspecialchars($cliente["edad"] ?? '') ?>" placeholder="" />
                         </div>
                     </div>
                     <div class="row mb-3">
@@ -57,7 +58,13 @@
                             <label><b>Provincia</b></label>
                         </div>                        
                         <div class="col-9">
-                            <input type="text" class="form-control" value="" placeholder="" />
+                            <select class="form-select">
+                                <?php foreach($provincias as $provincia): ?>
+                                    <option<?= ($cliente["provincia"] == $provincia ? ' selected' : '') ?>>
+                                        <?= htmlspecialchars($provincia) ?>
+                                    </option>
+                                <?php endforeach; ?>
+                            </select>
                         </div>
                     </div>  
                     <div class="row mb-3">
@@ -65,7 +72,7 @@
                             <label><b>Email</b></label>
                         </div>                        
                         <div class="col-9">
-                            <input type="text" class="form-control" value="" placeholder="" />
+                            <input type="email" name="email" class="form-control" value="<?= htmlspecialchars($cliente["email"] ?? '') ?>" placeholder="" />
                         </div>
                     </div>                                                                                                  
                 </form>
