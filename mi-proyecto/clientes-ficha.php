04a5a40b3e5484b6171c27d0273c24ddf70f86a0
Borja Cabeza <borja.cabeza@programadores.net>
2026-06-11 17:35:47 +0200

    Commit 11-06 19:35

--- mi-proyecto/clientes-ficha.php
+++ mi-proyecto/clientes-ficha.php
@@ -1,9 +1,83 @@
-<?php 
+<?php
+    require_once 'models/Customer.php';
+    require_once 'vendor/autoload.php';
+
+    use GuzzleHttp\Client;
+    use GuzzleHttp\Exception\RequestException;
+
     $tituloPagina = 'Ficha Cliente';
     $paginaActiva = 'fichacliente';
 
-    $id = $_GET['id'] ?? '';
+    $alerta = false;
+    $mensaje = '';
+    $tipoAlerta = '';
 
+    $baseUrl = 'https://gesnorthwind.azurewebsites.net/';
+
+    $client = new Client([
+        'base_uri' => $baseUrl,
+        'timeout' => 15
+    ]);
+
+    $headers = [
+        'apikey'        => '1234567890.',
+        'Content-Type'  => 'application/json', 
+        'Accept'        => 'application/json' 
+    ];  
+
+    if($_SERVER["REQUEST_METHOD"] == 'GET') {    
+        $id = $_GET['id'] ?? '';
+        $url = 'customers' . '/' . $id;
+
+        $response = $client->get($url, [
+            'headers' => $headers
+        ]);
+
+        if($response->getStatusCode() == 200) $data = json_decode($response->getBody(), true);    
+    } elseif ($_SERVER["REQUEST_METHOD"] == 'POST') {
+        $cliente = new Customer();
+
+        // Asignamos valores a las propiedades del objeto Cliente - Método 1
+        // foreach($_POST as $campo => $valor) {
+        //     if(property_exists($cliente, $campo)) {
+        //         $cliente->$campo = $valor;
+        //     }
+        // }
+
+        // Asignamos valores a las propiedades del objeto Cliente - Método 2
+        $cliente->customerID  = $_POST['customerID'];
+        $cliente->companyName  = $_POST['companyName'];
+        $cliente->contactName  = $_POST['contactName'];
+        $cliente->contactTitle  = $_POST['contactTitle'];
+        $cliente->address  = $_POST['address'];
+        $cliente->postalCode  = $_POST['postalCode'];
+        $cliente->city  = $_POST['city'];
+        $cliente->country  = $_POST['country'];
+        $cliente->region  = $_POST['region'];
+        $cliente->phone  = $_POST['phone'];
+        $cliente->fax  = $_POST['fax'];
+        
+        $url = 'customers' . '/' . $_POST['customerID'];
+
+        $response = $client->put($url, [
+            'headers'   => $headers,
+            'json'      => $cliente
+        ]);
+
+        if($response->getStatusCode() == 201) {
+            $data = (array)$cliente;
+
+            $alerta = true;
+            $mensaje = 'Ficha de ' . $cliente->companyName . ' actualizada correctamente.';
+            $tipoAlerta = 'alert-success';
+        } else {
+            $data = (array)$cliente;
+
+            $alerta = true;
+            $mensaje = 'Ficha de ' . $cliente->companyName . ' NO actualizada.';
+            $tipoAlerta = 'alert-danger';            
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -26,85 +100,97 @@
                     <!-- Card para el contenido -->
                     <div class="card shadow-sm card-custom mb-4">
                         <div class="card-header bg-white py-3">
-                            <h5 class="card-title m-0 text-secondary fw-semibold">Contenido Principal</h5>
+                            <h5 class="card-title m-0 text-secondary fw-semibold"><?= $data["companyName"] ?></h5>
                         </div>
+                        
+                        <?php if($alerta == true): ?>
+                        <div id="block-alert">
+                            <br />
+                            <div class="alert <?= $tipoAlerta ?>" role="alert">
+                                <?= $mensaje ?>
+                            </div>
+                            <br />
+                        </div>
+                        <?php endif; ?>
+
                         <div class="card-body bg-white">
                             <form method="post" action="">
     
                                 <div class="row mb-3">
                                     <label for="customerID" class="col-md-3 col-form-label text-end"><b>Identificador</b></label>
                                     <div class="col-md-9">
-                                        <input type="text" class="form-control" id="customerID" name="customerID" value="">
+                                        <input type="hidden" id="customerID-hidden" name="customerID" value="<?= $data["customerID"] ?>" />
+                                        <input type="text" class="form-control" id="customerID" name="customerID" value="<?= $data["customerID"] ?>" disabled />
                                     </div>
                                 </div>
 
                                 <div class="row mb-3">
                                     <label for="companyName" class="col-md-3 col-form-label text-end"><b>Empresa</b></label>
                                     <div class="col-md-9">
-                                        <input type="text" class="form-control" id="companyName" name="companyName" value="">
+                                        <input type="text" class="form-control" id="companyName" name="companyName" value="<?= $data["companyName"] ?>" />
                                     </div>
                                 </div>
 
                                 <div class="row mb-3">
                                     <label for="contactName" class="col-md-3 col-form-label text-end"><b>Responsable</b></label>
                                     <div class="col-md-9">
-                                        <input type="text" class="form-control" id="contactName" name="contactName" value="">
+                                        <input type="text" class="form-control" id="contactName" name="contactName" value="<?= $data["contactName"] ?>" />
                                     </div>
                                 </div>
 
                                 <div class="row mb-3">
                                     <label for="contactTitle" class="col-md-3 col-form-label text-end"><b>Cargo</b></label>
                                     <div class="col-md-9">
-                                        <input type="text" class="form-control" id="contactTitle" name="contactTitle" value="">
+                                        <input type="text" class="form-control" id="contactTitle" name="contactTitle" value="<?= $data["contactTitle"] ?>" />
                                     </div>
                                 </div>
 
                                 <div class="row mb-3">
                                     <label for="address" class="col-md-3 col-form-label text-end"><b>Dirección</b></label>
                                     <div class="col-md-9">
-                                        <input type="text" class="form-control" id="address" name="address" value="">
+                                        <input type="text" class="form-control" id="address" name="address" value="<?= $data["address"] ?>" />
                                     </div>
                                 </div>
 
                                 <div class="row mb-3">
                                     <label for="city" class="col-md-3 col-form-label text-end"><b>Ciudad</b></label>
                                     <div class="col-md-9">
-                                        <input type="text" class="form-control text-end" id="city" name="city" value="">
+                                        <input type="text" class="form-control" id="city" name="city" value="<?= $data["city"] ?>" />
                                     </div>
                                 </div>
 
                                 <div class="row mb-3">
                                     <label for="region" class="col-md-3 col-form-label text-end"><b>Region</b></label>
                                     <div class="col-md-9">
-                                        <input type="text" class="form-control" id="region" name="region" value="">
+                                        <input type="text" class="form-control" id="region" name="region" value="<?= $data["region"] ?>" />
                                     </div>
                                 </div>
 
                                 <div class="row mb-3">
                                     <label for="postalCode" class="col-md-3 col-form-label text-end"><b>Código Postal</b></label>
                                     <div class="col-md-9">
-                                        <input type="text" class="form-control" id="postalCode" name="postalCode" value="">
+                                        <input type="text" class="form-control" id="postalCode" name="postalCode" value="<?= $data["postalCode"] ?>" />
                                     </div>
                                 </div>
 
                                 <div class="row mb-3">
                                     <label for="country" class="col-md-3 col-form-label text-end"><b>País</b></label>
                                     <div class="col-md-9">
-                                        <input type="text" class="form-control" id="country" name="country" value="">
+                                        <input type="text" class="form-control" id="country" name="country" value="<?= $data["country"] ?>" />
                                     </div>
                                 </div>
 
                                 <div class="row mb-3">
                                     <label for="phone" class="col-md-3 col-form-label text-end"><b>Teléfono</b></label>
                                     <div class="col-md-9">
-                                        <input type="text" class="form-control" id="phone" name="phone" value="">
+                                        <input type="text" class="form-control" id="phone" name="phone" value="<?= $data["phone"] ?>" />
                                     </div>
                                 </div>
 
                                 <div class="row mb-3">
                                     <label for="fax" class="col-md-3 col-form-label text-end"><b>Fax</b></label>
                                     <div class="col-md-9">
-                                        <input type="text" class="form-control" id="fax" name="fax" value="">
+                                        <input type="text" class="form-control" id="fax" name="fax" value="<?= $data["fax"] ?>" />
                                     </div>
                                 </div>
                                 
@@ -142,5 +228,12 @@
     </div>
 
     <?php require_once __DIR__ . '/layouts/scripts.php'; ?>
+    <script>
+        $(documet).ready(function() {
+            setTimeout(function() {
+                $('#block-alert').fadeOut()
+            }, 3000);
+        });
+    </script>
 </body>
 </html>
